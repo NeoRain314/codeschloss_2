2655a6ca2ad0939311faac54c7315d74e031d994
NeoRain314 <106302257+NeoRain314@users.noreply.github.com>
2022-07-28 09:47:24 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,51 @@
+let locked = false
+let myInput: number[] = []
+let code = [0, 1, 2, 3]
+myInput = [1, 0]
+locked = true
+
+// Code     //
+clearInput()
+
+
+
+// Code Eingabe     //
+input.onPinPressed(TouchPin.P0, function () {
+    if (locked) {
+        myInput.push(0)
+    }
+})
+input.onPinPressed(TouchPin.P3, function () {
+    if (locked) {
+        myInput.push(3)
+    }
+})
+input.onPinPressed(TouchPin.P2, function () {
+    if (locked) {
+        myInput.push(2)
+    }
+})
+input.onPinPressed(TouchPin.P1, function () {
+    if (locked) {
+        myInput.push(1)
+    }
+})
+
+input.onButtonPressed(Button.A, function () {
+    if (locked) {
+        checkCode()
+    }
+})
+
+// Hilfsfunktionen     //
+
+function clearInput () {
+    for (let index = 0; index < myInput.length; index++) {
+        myInput.pop()
+    }
+}
+
+function checkCode() {
+    if(code = myInput)
+}
+
